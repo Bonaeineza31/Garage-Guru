89498a5c8b43cfe095daa2e9eac4107cd32c4f71
atete <n.atete@alustudent.com>
2026-03-27 09:45:10 +0200

    Connected branch with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:garage_guru/firebase_options.dart';
 import 'package:garage_guru/core/theme/app_theme.dart';
 import 'package:garage_guru/screens/auth/landing_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
