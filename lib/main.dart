43a3d6ae97fc09128da86ef05b7d84b27b117592
Ntare-GAMA <a.ntare@alustudent.com>
2026-04-01 11:34:56 +0200

    Initialize Firebase in main.dart using firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:garage_guru/core/theme/theme_data.dart';
 import 'package:garage_guru/screens/auth/landing_screen.dart';
 import 'package:garage_guru/core/theme/app_theme.dart';
 import 'package:garage_guru/screens/auth/login_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
